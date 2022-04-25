5d2a48c50519b1e1f4cae3766c2497ed4985175d
Jools Arts <jools.arts@gmail.com>
2022-04-25 12:55:03 +0100

    Added annotations to both my Paleontolgist and Dinosaur Java classes

--- src/main/java/com/qa/hobbywebapplication/controller/DinosaurController.java
+++ src/main/java/com/qa/hobbywebapplication/controller/DinosaurController.java
@@ -1,5 +1,12 @@
 package com.qa.hobbywebapplication.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/dinosaur")
+@CrossOrigin("*")
 public class DinosaurController {
 
 }
