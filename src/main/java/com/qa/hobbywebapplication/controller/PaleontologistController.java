6ac2a3b25d2498b58d3881a3caf16041d1c0e7e8
Jools Arts <jools.arts@gmail.com>
2022-04-25 15:09:24 +0100

    Created a new application-dev.properties file which allows for configuration of JPA and SQL

--- src/main/java/com/qa/hobbywebapplication/controller/PaleontologistController.java
+++ src/main/java/com/qa/hobbywebapplication/controller/PaleontologistController.java
@@ -1,12 +1,22 @@
 package com.qa.hobbywebapplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.hobbywebapplication.service.PaleontologistService;
 
 @RestController
 @RequestMapping(path = "/Paleontologist")
 @CrossOrigin("*")
 public class PaleontologistController {
+	
+	private PaleontologistService paleontologistService;
+	
+	@Autowired
+	public PaleontologistController (PaleontologistService paleontologistService) {
+		this.paleontologistService = paleontologistService;
+	}
 
 }
---
+++ src/main/java/com/qa/hobbywebapplication/service/PaleontologistService.java
@@ -0,0 +1,8 @@
+package com.qa.hobbywebapplication.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaleontologistService {
+
+}
