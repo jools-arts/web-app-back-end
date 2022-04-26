4547c3402c25cbe827513f78d630702cf97c21cd
Jools Arts <jools.arts@gmail.com>
2022-04-26 09:00:24 +0100

    Updated the file structure concerning my SQL schema

--- src/main/resources/application-schema.sql
+++ src/main/resources/application-schema.sql
@@ -1,7 +1,27 @@
 USE hobby_web_application;
 
-drop table if exists dinosaur
-drop table if exists paleontologist
-create table dinosaur (id integer not null auto_increment, diet varchar(50) not null, habitat varchar(50) not null, period varchar(50) not null, region varchar(50) not null, species varchar(50) not null, paleontologist_id integer, primary key (id)) engine=InnoDB
-create table paleontologist (id integer not null auto_increment, email_address varchar(50) not null, forename varchar(50) not null, institution varchar(50) not null, specialism varchar(50) not null, surname varchar(50) not null, username varchar(50) not null, primary key (id)) engine=InnoDB
+DROP TABLE IF EXISTS `hobby_web_application`.`paleotologist`;
+DROP TABLE IF EXISTS `hobby_web_application`.`dinosaur`;
+
+CREATE TABLE `paleontologist` (
+	`id` INTEGER NOT NULL AUTO_INCREMENT, 
+	`email_address` VARCHAR(50) NOT NULL, 
+	`forename` VARCHAR(50) NOT NULL, 
+	`institution` VARCHAR(50) NOT NULL, 
+	`specialism` VARCHAR(50) NOT NULL, 
+	`surname` VARCHAR(50) NOT NULL, 
+	`username` VARCHAR(50) NOT NULL, 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE `hobby_web_application`.`dinosaur` (
+	`id` integer not null auto_increment, 
+    `diet` varchar(50) not null, 
+    `habitat` varchar(50) not null, 
+    `period` varchar(50) not null, 
+    `region` varchar(50) not null, 
+    `species` varchar(50) not null, paleontologist_id integer, 
+    PRIMARY KEY (id)
+);
+
 alter table dinosaur add constraint FKmfg8s4d7dqtrtm6hqop4cflsq foreign key (paleontologist_id) references paleontologist (id)
