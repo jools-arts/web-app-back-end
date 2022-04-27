b00e6f5bac0db57df3519d7e919581ca6989b39c
Jools Arts <jools.arts@gmail.com>
2022-04-27 10:12:53 +0100

    Upadted my FossilSiteService class so it could return a list of fossil sites

--- src/main/java/com/qa/hobbywebapplication/service/FossilSiteService.java
+++ src/main/java/com/qa/hobbywebapplication/service/FossilSiteService.java
@@ -1,5 +1,26 @@
 package com.qa.hobbywebapplication.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.hobbywebapplication.data.entity.FossilSite;
+import com.qa.hobbywebapplication.data.repository.FossilSiteRepository;
+
+@Service
 public class FossilSiteService {
+	
+	private FossilSiteRepository fossilSiteRepository;
+	
+	@Autowired
+	public FossilSiteService(FossilSiteRepository fossilSiteRepository) {
+		super();
+		this.fossilSiteRepository = fossilSiteRepository;
+	}
+	
+	public List<FossilSite> getFossilSites() {
+		return fossilSiteRepository.findAll();
+	}
 
 }
