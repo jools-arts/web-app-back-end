4b77f876c328592681a95833989024b5a29f6b7c
Jools Arts <jools.arts@gmail.com>
2022-04-27 10:02:16 +0100

    Updated my PaleontologistService class

--- src/main/java/com/qa/hobbywebapplication/service/PaleontologistService.java
+++ src/main/java/com/qa/hobbywebapplication/service/PaleontologistService.java
@@ -1,8 +1,11 @@
 package com.qa.hobbywebapplication.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.qa.hobbywebapplication.data.entity.Paleontologist;
 import com.qa.hobbywebapplication.data.repository.PaleontologistRepository;
 
 @Service
@@ -14,6 +17,10 @@ public class PaleontologistService {
 	public PaleontologistService(PaleontologistRepository paleontologistRepository) {
 		super();
 		this.paleontologistRepository = paleontologistRepository;
+	}
+	
+	public List<Paleontologist> getPaleontologists() {
+		return paleontologistRepository.findAll();
 	}
 
 }
