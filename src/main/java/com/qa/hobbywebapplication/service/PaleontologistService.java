b1d5f52d85436c97da9f350f5e69e1b613c4dc2b
Jools Arts <jools.arts@gmail.com>
2022-04-27 11:27:20 +0100

    provided extra functionality for my PaleontologistService class

--- src/main/java/com/qa/hobbywebapplication/service/PaleontologistService.java
+++ src/main/java/com/qa/hobbywebapplication/service/PaleontologistService.java
@@ -1,26 +1,42 @@
 package com.qa.hobbywebapplication.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qa.hobbywebapplication.data.entity.Paleontologist;
 import com.qa.hobbywebapplication.data.repository.PaleontologistRepository;
+import com.qa.hobbywebapplication.dto.PaleontologistDTO;
 
 @Service
 public class PaleontologistService {
 	
 	private PaleontologistRepository paleontologistRepository;
+	private FossilSiteService fossilSiteService;
+	private ModelMapper modelmapper;
 	
 	@Autowired
-	public PaleontologistService(PaleontologistRepository paleontologistRepository) {
+	public PaleontologistService(PaleontologistRepository paleontologistRepository, FossilSiteService fossilSiteService, ModelMapper modelMapper) {
 		super();
 		this.paleontologistRepository = paleontologistRepository;
+		this.fossilSiteService = fossilSiteService;
+		this.modelMapper = modelMapper;
 	}
 	
-	public List<Paleontologist> getPaleontologists() {
-		return paleontologistRepository.findAll();
+	public List<PaleontologistDTO> getPaleontologists() {
+		List<Paleontologist> paleontologists = paleontologistRepository.findAll();
+		List<PaleontologistDTO> paleontologistDTOs = new ArrayList<>();
+		
+		for (Paleontologist paleontologist: paleontologists) {
+			paleontologistDTOs.add(this.toDTO(paleontologist));
+		}
+		return paleontologistDTOs;
 	}
-
+	
+	private PaleontologistDTO toDTO(Paleontologist paleontologist) {
+		return this.modelmapper.map(paleontologist, PaleontologistDTO.class);
+	}
 }
