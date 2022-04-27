ebf235bc137e60816c2ec9eecc907601f5768a63
Jools Arts <jools.arts@gmail.com>
2022-04-27 08:10:22 +0100

    Updated my repository classes so they extend the JpaRepository

--- src/main/java/com/qa/hobbywebapplication/data/repository/FossilSiteRepository.java
+++ src/main/java/com/qa/hobbywebapplication/data/repository/FossilSiteRepository.java
@@ -1,5 +1,11 @@
 package com.qa.hobbywebapplication.data.repository;
 
-public interface FossilSiteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.hobbywebapplication.data.entity.FossilSite;
+
+@Repository
+public interface FossilSiteRepository extends JpaRepository<FossilSite, Integer> {
 
 }
