58467f441f6cf952cfa0a92a51cbb478882af986
Jools Arts <jools.arts@gmail.com>
2022-04-27 10:08:15 +0100

    Updated my DinosaurService class so it could return a list of dinosaurs

--- src/main/java/com/qa/hobbywebapplication/service/DinosaurService.java
+++ src/main/java/com/qa/hobbywebapplication/service/DinosaurService.java
@@ -1,5 +1,26 @@
 package com.qa.hobbywebapplication.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.hobbywebapplication.data.entity.Dinosaur;
+import com.qa.hobbywebapplication.data.repository.DinosaurRepository;
+
+@Service
 public class DinosaurService {
+	
+	private DinosaurRepository dinosaurRepository;
+	
+	@Autowired
+	public DinosaurService(DinosaurRepository dinosaurRepository) {
+		super();
+		this.dinosaurRepository = dinosaurRepository;
+	}
+	
+	public List<Dinosaur> getDinosaurs() {
+		return dinosaurRepository.findAll();
+	}
 
 }
