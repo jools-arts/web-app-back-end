0b3c3752072bc81be8eb2bb94a59e71c5714ace9
Jools Arts <jools.arts@gmail.com>
2022-04-29 00:48:46 +0100

    Added a createDinosaur method to my DinosaurController class

--- src/main/java/com/qa/hobbywebapplication/controller/DinosaurController.java
+++ src/main/java/com/qa/hobbywebapplication/controller/DinosaurController.java
@@ -2,13 +2,17 @@ package com.qa.hobbywebapplication.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +43,16 @@ public class DinosaurController {
 	public ResponseEntity<DinosaurDTO> getDinosaur(@PathVariable(name = "dinosaur_id") int dinosaurId) {
 		DinosaurDTO dinosaur = dinosaurService.getDinosaur(dinosaurId);
 		return new ResponseEntity<>(dinosaur, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<DinosaurDTO> createDinosaur(@Valid @RequestBody NewDinosaurDTO dinosaur) {
+		DinosaurDTO newDinosaur = dinosaurService.createDinosaur(dinosaur);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Location", "http://localhost:8080/dinosaur/" + newDinosaur.getDinosaurId());
+		
+		return new ResponseEntity<>(newDinosaur, headers, HttpStatus.CREATED);
 	}
 	
 	@PutMapping(path = "/{dinosaur_id}")
