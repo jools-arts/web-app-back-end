62fe286f7a6617c8a6ad1561f7d623705c4f909c
Jools Arts <jools.arts@gmail.com>
2022-04-28 10:03:37 +0100

    Updated my DinosaurController class with Get, Put, and Delete mapping

--- src/main/java/com/qa/hobbywebapplication/controller/DinosaurController.java
+++ src/main/java/com/qa/hobbywebapplication/controller/DinosaurController.java
@@ -1,12 +1,56 @@
 package com.qa.hobbywebapplication.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.hobbywebapplication.dto.DinosaurDTO;
+import com.qa.hobbywebapplication.dto.NewDinosaurDTO;
+import com.qa.hobbywebapplication.service.DinosaurService;
 
 @RestController
 @RequestMapping(path = "/dinosaur")
 @CrossOrigin("*")
 public class DinosaurController {
+	
+	private DinosaurService dinosaurService;
+	
+	@Autowired
+	public DinosaurController (DinosaurService dinosaurService) {
+		this.dinosaurService = dinosaurService;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<DinosaurDTO>> getDinosaurs() {
+		return ResponseEntity.ok(dinosaurService.getDinosaurs());
+	}
+	
+	@GetMapping(path = "/{dinosaur_id}")
+	public ResponseEntity<DinosaurDTO> getDinosaur(@PathVariable(name = "dinosaur_id") int dinosaurId) {
+		DinosaurDTO dinosaur = dinosaurService.getDinosaur(dinosaurId);
+		return new ResponseEntity<>(dinosaur, HttpStatus.OK);
+	}
+	
+	@PutMapping(path = "/{dinosaur_id}")
+	public ResponseEntity<DinosaurDTO> updateDinosaur(@RequestBody NewDinosaurDTO newDinosaurDTO, @PathVariable(name = "dinosaur_id") int dinosaurId) {
+		return ResponseEntity.ok(dinosaurService.updateDinosaur(newDinosaurDTO, dinosaurId));
+	}
+	
+	@DeleteMapping(path = "/{dinosaur_id")
+	public ResponseEntity<?> deleteDinosaur(@PathVariable(name = "dinosaur_id") int dinosaurId) {
+		DinosaurDTO deletedDinosaur = dinosaurService.getDinosaur(dinosaurId);
+		dinosaurService.deleteDinosaur(dinosaurId);
+		return ResponseEntity.ok(deletedDinosaur);
+	}
 
 }
--- src/main/java/com/qa/hobbywebapplication/controller/PaleontologistController.java
+++ src/main/java/com/qa/hobbywebapplication/controller/PaleontologistController.java
@@ -65,4 +65,5 @@ public class PaleontologistController {
 		paleontologistService.deletePaleontologist(paleontologistId);
 		return ResponseEntity.ok(deletedPaleontologist);
 	}
+	
 }
